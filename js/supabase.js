71917e9074eaf9a2ce8680ee035cace9a5586926
Amat Besari <amatuchiha@gmail.com>
2026-08-05 09:28:56 +0700

    update supabase key

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const SUPABASE_URL = 'GANTI_DENGAN_PROJECT_URL_KAMU'
-const SUPABASE_ANON_KEY = 'GANTI_DENGAN_ANON_KEY_KAMU'
+const SUPABASE_URL = 'https://jqhiroygophpabzxyiyx.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxaGlyb3lnb3BocGFienh5aXl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4ODAzMDksImV4cCI6MjEwMTQ1NjMwOX0.NwPlYOkbGUsoifwTC32yVoJhDp29CNkyVS_ZVcQGt_s'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
